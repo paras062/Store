048264b0eb0923943667eca488aed2a4a90c6034
Paras Babbar <parasbabbar77@gmail.com>
2018-06-28 11:42:22 -0700

    Structure Created

--- Store.DataModels/Properties/AssemblyInfo.cs
+++ Store.DataModels/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StoreBusinessModels")]
+[assembly: AssemblyTitle("Store.DataModels")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StoreBusinessModels")]
+[assembly: AssemblyProduct("Store.DataModels")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7e009266-51f1-4084-86c2-36ffc0fc24df")]
+[assembly: Guid("25ef03b4-1be2-416d-9c86-67ba39bb7641")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Store.DataModels/StoreInventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.DataModels
+{
+    public class StoreInventory
+    {
+        public int Id { get; set; }
+        public string ContentName { get; set; }
+        public int ContentQuantity { get; set; }
+    }
+}
